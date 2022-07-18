dddf75201245e5bf99dcbda424610478943b02c9
AlfinSib <s2150442@ed.ac.uk>
2022-07-18 19:48:56 +0100

    Mix MixtureSample with NormalSample to get center

--- Implementation.py
+++ Implementation.py
@@ -11,19 +11,13 @@ np.random.seed(0)
 e = math.e
 pi = math.pi
 
-def F_1(z, Flag = 1): 
-    if Flag == 1:
-        r = abs(z - RandomMixtureSample) # Random point in the cluster
-    else:
-        r = abs(z - RandomNormalSample)
+def F_1(z): 
+    r = abs(z - RandomSample)# Random point in the cluster
     alpha = 1.5 # Consistent with the density (As this gets larger, less samples are moved close to 0)
     return r * math.erf(r/alpha) + (alpha/math.sqrt(pi)) * math.pow(e, -(r/alpha) ** 2)
 
-def F_2(z, Flag = 1):
-    if Flag == 1:
-        r = abs(z - RandomMixtureSample)
-    else:
-        r = abs(z - RandomNormalSample)
+def F_2(z):
+    r = abs(z - RandomSample)
     alpha = 1.5
     return alpha + r - alpha * math.log(abs(alpha + r))
 
@@ -32,9 +26,9 @@ def Beta_1Calculation():  # Through gradient descent
     xSummationDerivative = 0
     ySummationDerivative = 0
     for j in range(0, len(StandardNormalSamples)):
-        ySummationDerivative += F_1(StandardNormalSamples[j], 2)
+        ySummationDerivative += F_1(StandardNormalSamples[j])
     for i in range(0, len(MixtureSamples)):
-        xSummationDerivative += F_1(MixtureSamples[i], 1)
+        xSummationDerivative += F_1(MixtureSamples[i])
     Beta_1 = (-1/len(MixtureSamples)) * xSummationDerivative + \
         (1/len(StandardNormalSamples)) * ySummationDerivative
     return Beta_1 * Proportion
@@ -44,9 +38,9 @@ def Beta_2Calculation():
     xSummationDerivative = 0
     ySummationDerivative = 0
     for j in range(0, len(StandardNormalSamples)):
-        ySummationDerivative += F_2(StandardNormalSamples[j], 2)
+        ySummationDerivative += F_2(StandardNormalSamples[j])
     for i in range(0, len(MixtureSamples)):
-        xSummationDerivative += F_2(MixtureSamples[i], 1)
+        xSummationDerivative += F_2(MixtureSamples[i])
     Beta_2 = (-1/len(MixtureSamples)) * xSummationDerivative + \
         (1/len(StandardNormalSamples)) * ySummationDerivative
     return Beta_2 * Proportion
@@ -54,7 +48,7 @@ def Beta_2Calculation():
 
 
 def u(x, Beta_1, Beta_2):
-    return (x ** 2 / 2) + Beta_1 * F_1(x, 1) + Beta_2 * F_2(x, 1)
+    return (x ** 2 / 2) + Beta_1 * F_1(x) + Beta_2 * F_2(x)
 
 
 def uConjugate(y, Beta_1, Beta_2):
@@ -106,13 +100,12 @@ def SamplesUpdate(MixtureSamples):
 
 StandardNormalSamples = np.random.standard_normal(100)
 MixtureSamples = MixtureSampleGenerator()
-
+CenterGeneratorList = StandardNormalSamples + MixtureSamples
 
 steps = [MixtureSamples]
 
-for i in range(0, 50): # Maybe there is a problem of overfitting
-    RandomMixtureSample = MixtureSamples[random.randint(0,len(MixtureSamples) - 1)]
-    RandomNormalSample = StandardNormalSamples[random.randint(0,len(StandardNormalSamples) - 1)]
+for i in range(0, 20): # Maybe there is a problem of overfitting
+    RandomSample = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
     Beta_1 = Beta_1Calculation()
     Beta_2 = Beta_2Calculation()
     MixtureSamples = SamplesUpdate(MixtureSamples)
