e4adcca3e7b6ebf43d3b1db96511bbfaad8711f6
Anupam Bhakta <anupambhakta1@gmail.com>
2022-07-19 23:18:56 +0530

    added some boilerplate for integrated rf(r)

--- family_of_f.py
+++ family_of_f.py
@@ -5,18 +5,14 @@ e = math.e
 pi = math.pi
 
 class Potentialf:
+    # Find differentiated version of bump_F
     def bump_f(z, center):
-        value = 0
-        r = abs(z - center)
-        alpha = 0.5  # Consistent with the density 
-        if r < 1/alpha:
-            value = math.pow(e, -(1/(1-(alpha*r)**2)))
-        return value
+        return
     
     def giulio_f(z, center):
         r = abs(z - center)
-        alpha = 1.5  # Consistent with the density 
-        return r * math.erf(r/alpha) + (alpha/math.sqrt(pi)) * math.pow(e, -(r/alpha) ** 2)
+        alpha = 1.5
+        return math.erf(r/alpha) / r
     
     def gaussian_f(z, z_center):
         r = abs(z - z_center)
@@ -49,4 +45,34 @@ class Potentialf:
         r = abs(z - z_center)
         return r**2*np.log(r)
     
+class PotentialF:
+    def bump_F(z, center):
+        value = 0
+        r = abs(z - center)
+        alpha = 0.5
+        if r < 1/alpha:
+            value = math.pow(e, -(1/(1-(alpha*r)**2)))
+        return value
     
+    def giulio_F(z, center):
+        r = abs(z - center)
+        alpha = 1.5  # Consistent with the density 
+        return r * math.erf(r/alpha) + (alpha/math.sqrt(pi)) * math.pow(e, -(r/alpha) ** 2)
+    
+    def gaussian_F(z, z_center):
+        return
+    
+    def multiquadric_F(z, z_center):
+        return
+    
+    def inverseQuadratic_F(z, z_center):
+        return
+    
+    def inverseMultiquadric_F(z, z_center):
+        return
+    
+    def polyharmonicSpline_F(z, z_center, k):
+        return
+    
+    def thinPlateSpline_F(z, z_center):
+        return
