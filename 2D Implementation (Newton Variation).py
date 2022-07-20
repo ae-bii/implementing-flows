04a63d9ad894c869d69c1559d9365ef04568a1ba
AlfinSib <s2150442@ed.ac.uk>
2022-07-20 19:33:31 +0100

    Add a version with Newton's method

---
+++ 2D Implementation (Newton Variation).py
@@ -0,0 +1,142 @@
+import numpy as np
+import random
+import math
+import numdifftools as nd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+e = math.e
+pi = math.pi
+
+def distance(z1, z2):
+    sum = 0
+    for i in range(0,len(z1)-1):
+        sum += (z1[i] - z2[i]) ** 2
+    return math.sqrt(sum)
+
+def F_1(z):
+    r = distance(z, center1)
+    alpha = 0.5 # Consistent with the density (As this gets larger, less samples are moved close to 0)
+    return r * math.erf(r/alpha) + (alpha/math.sqrt(pi)) * math.pow(e, -(r/alpha) ** 2)
+
+def F_2(z):
+    r = distance(z, center2)
+    alpha = 0.5
+    return alpha + r - alpha * math.log(abs(alpha + r))
+
+def BetaNewton(): # Newton's method (Experimental)
+    H = nd.Hessian(D)([0, 0])
+    G = nd.Gradient(D)([0, 0])
+    HInverseNeg = (-1) * np.linalg.inv(H)
+    Beta = np.matmul(HInverseNeg, G)
+    ParameterList = [1, 10/np.linalg.norm(Beta)]
+    return Beta * min(ParameterList) # min(ParameterList) can be understood as similar to the "Proportion" in gradient descent
+
+def u(x, Beta_1, Beta_2):
+    return (((x[0] ** 2) + (x[1] ** 2)) / 2) + Beta_1 * F_1(x) + Beta_2 * F_2(x)
+
+def uConjugate(y, Beta_1, Beta_2):
+    ConvexCandidate = []
+    for i in range(0, len(MixtureSample)):
+        ConjugateValue = np.dot(MixtureSample[i], y) - u(MixtureSample[i], Beta_1, Beta_2)
+        ConvexCandidate.append(ConjugateValue)
+    return max(ConvexCandidate)
+
+def D(Beta):
+    xSummation = 0
+    ySummation = 0
+    for i in range(0, len(MixtureSample)):
+        xSummation += u(MixtureSample[i], Beta[0], Beta[1])
+    for j in range(0, len(StandardNormal)):
+        ySummation += uConjugate(StandardNormal[j], Beta[0], Beta[1])
+
+    LL = 1/len(MixtureSample) * xSummation + 1 / \
+        len(StandardNormal) * ySummation
+
+    return LL
+
+
+
+def SamplesUpdate(OldMixtureSample):
+    NewMixtureSample = []
+    for i in range(0, len(OldMixtureSample)):
+        xval = OldMixtureSample[i][0] + Beta_1 * nd.Gradient(F_1)(OldMixtureSample[i])[0] + Beta_2 * nd.Gradient(F_2)(OldMixtureSample[i])[0]
+        yval = OldMixtureSample[i][1] + Beta_1 * nd.Gradient(F_1)(OldMixtureSample[i])[1] + Beta_2 * nd.Gradient(F_2)(OldMixtureSample[i])[1]
+        NewMixtureSample.append([xval,yval])
+    NewMixtureSample = np.array(NewMixtureSample)
+    return NewMixtureSample
+
+def MixtureSampleGenerator():
+    mean1 = [1, -1]
+    cov1 = [[0.5, 0], [0, 0.5]]
+    mean2 = [-1, 1]
+    cov2 = [[0.5, 0], [0, 0.5]]
+    x = np.random.multivariate_normal(mean1, cov1, 50)
+    y = np.random.multivariate_normal(mean2, cov2, 50)
+    MixtureSample = []
+    for i in range(50):
+        RandomSelector = random.random()
+        if RandomSelector > 0.7:
+            MixtureSample.append(x[i])
+        else:
+            MixtureSample.append(y[i])
+    MixtureSample = np.array(MixtureSample)
+    return MixtureSample
+
+def StandardNormalGenerator():
+    Sample = []
+    x = np.random.standard_normal(50)
+    y = np.random.standard_normal(50)
+    for i in range(50):
+        Sample.append([x[i], y[i]])
+    return Sample
+
+
+#------------------------------------------------------------------ TESTING (change to heatmap, add animtation)------------------------------------------------------------
+MixtureSample = MixtureSampleGenerator()
+StandardNormal = StandardNormalGenerator()
+CenterGeneratorList = MixtureSample + StandardNormal
+
+plt.subplot(1,3,3)
+plt.title("Target")
+plt.scatter(*zip(*StandardNormal), color = 'r', alpha = 0.2)
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+
+plt.subplot(1,3,1)
+plt.title("Initial")
+plt.scatter(*zip(*MixtureSample), color = 'b', alpha = 0.2)
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+
+DValue = 0
+Iteration = 0
+while True: # Maybe there is a problem of overfitting
+    #print("Iteration " + str(i))
+    Iteration += 1
+    CenterSelector = 0
+    center1 = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
+    center2 = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)] # Choose 2 different centers for different Fs (Experimental)
+    while CenterSelector == 0 and Iteration >= 2:
+        if np.linalg.norm(np.subtract(center1,center2)) <= 1:
+            center1 = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
+        else:
+            CenterSelector = 1
+    Beta = BetaNewton()
+    print(Beta)
+    Beta_1 = Beta[0]
+    Beta_2 = Beta[1]
+    OldD = DValue
+    DValue = D(Beta)
+    print(DValue)
+    MixtureSample = SamplesUpdate(MixtureSample)
+    if abs(DValue - OldD) < 0.0001 or Iteration > 25:
+        break
+
+plt.subplot(1,3,2)
+plt.title("Optimal Transport")
+plt.scatter(*zip(*MixtureSample), color = 'g', alpha = 0.2)
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+plt.show()
