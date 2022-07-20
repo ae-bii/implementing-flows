1a96b4f5cb103753444d5cf20659c3513989039c
Anupam Bhakta <anupambhakta1@gmail.com>
2022-07-20 17:04:50 +0530

    renamed z_center to center for uniformity

--- family_of_f.py
+++ family_of_f.py
@@ -20,35 +20,35 @@ class Potentialf:
         alpha = 1.5
         return math.erf(r/alpha) / r
     
-    def gaussian_f(z, z_center):
-        r = abs(z - z_center)
+    def gaussian_f(z, center):
+        r = abs(z - center)
         alpha = 1
         return math.pow(e, -((alpha*r)**2))
     
-    def multiquadric_f(z, z_center):
-        r = abs(z - z_center)
+    def multiquadric_f(z, center):
+        r = abs(z - center)
         alpha = 1
         return math.sqrt(1+(alpha*r)**2)
     
-    def inverseQuadratic_f(z, z_center):
-        r = abs(z - z_center)
+    def inverseQuadratic_f(z, center):
+        r = abs(z - center)
         alpha = 1
         return 1/(1+(alpha*r)**2)
     
-    def inverseMultiquadric_f(z, z_center):
-        r = abs(z - z_center)
+    def inverseMultiquadric_f(z, center):
+        r = abs(z - center)
         alpha = 1
         return 1/math.sqrt(1+(alpha*r)**2)
     
-    def polyharmonicSpline_f(z, z_center, k):
-        r = abs(z - z_center)
+    def polyharmonicSpline_f(z, center, k):
+        r = abs(z - center)
         if k%2 == 0:
             return r**(k-1)*np.log(r**r) 
         else:
             return r**k
     
-    def thinPlateSpline_f(z, z_center):
-        r = abs(z - z_center)
+    def thinPlateSpline_f(z, center):
+        r = abs(z - center)
         return r**2*np.log(r)
     
 class PotentialF:
@@ -65,29 +65,29 @@ class PotentialF:
         return r * math.erf(r/alpha) + (alpha/math.sqrt(pi)) * math.pow(e, -(r/alpha) ** 2)
     
     
-    def gaussian_F(z, z_center, alpha, constant):
-        r = abs(z - z_center)
+    def gaussian_F(z, center, alpha, constant):
+        r = abs(z - center)
         return -math.pow(e, -alpha**2 * r**2) / (2 * alpha**2) + constant
     
-    def multiquadric_F(z, z_center, alpha, constant):
-        r = abs(z - z_center)
+    def multiquadric_F(z, center, alpha, constant):
+        r = abs(z - center)
         return math.pow(alpha**2 * r**2 + 1, 3/2) / (3 * alpha**2) + constant
     
-    def inverseQuadratic_F(z, z_center, alpha, constant):
-        r = abs(z - z_center)
+    def inverseQuadratic_F(z, center, alpha, constant):
+        r = abs(z - center)
         return math.log(alpha**2 * r**2 + 1) / (2 * alpha**2) + constant
     
-    def inverseMultiquadric_F(z, z_center, alpha, constant):
-        r = abs(z - z_center)
+    def inverseMultiquadric_F(z, center, alpha, constant):
+        r = abs(z - center)
         return math.sqrt(alpha**2 * r**2 + 1) / (alpha**2) + constant
     
-    def polyharmonicSpline_F(z, z_center, constant, k):
-        r = abs(z - z_center)
+    def polyharmonicSpline_F(z, center, constant, k):
+        r = abs(z - center)
         if k%2 == 0:
             return (r**(k+1) * ((k+2) * math.log(r) - 1)) / ((k+2)**2)  + constant #Since k and r are positive
         else:
             return r**(k+2) / (k+2) + constant
     
-    def thinPlateSpline_F(z, z_center, constant):
-        r = abs(z - z_center)
+    def thinPlateSpline_F(z, center, constant):
+        r = abs(z - center)
         return r**4 * (math.log(r)/4 - 1/16) + constant
