9d0ff06a8d4a76327d7ac9f1638c7b907b98bef9
Anupam Bhakta <anupambhakta1@gmail.com>
2022-07-21 22:46:34 +0530

    implemented setters for all in PotentialF

--- family_of_f.py
+++ family_of_f.py
@@ -59,54 +59,53 @@ class PotentialF:
         self._alpha = alpha
         self._constant = constant
         self._k = k
-        
+
     def set_center(self, center):
         self._center = center
-        
+
     def set_alpha(self, alpha):
         self._alpha = alpha
-        
+
     def set_constant(self, constant):
         self._constant = constant
-        
+
     def set_k(self, k):
         self._k = k
-    
-    def bump_F(z, center, alpha, constant):
-        r = distance(z, center)
-        if r < 1/alpha:
-            return (math.exp(1 / (alpha**2 * r**2 - 1)) * (alpha**2 * r**2 - 1) - expi(1 / (alpha**2 * r**2 - 1))) / (2 * alpha**2) + constant
-        return r + constant
 
-    # Testing if setter method works
-    def giulio_F(self, z):
+    def bump_F(self, z):
         r = distance(z, self._center)
+        if r < 1/self._alpha:
+            return (math.exp(1 / (self._alpha**2 * r**2 - 1)) * (self._alpha**2 * r**2 - 1) - expi(1 / (self._alpha**2 * r**2 - 1))) / (2 * self._alpha**2) + self._constant
+        return r + self._constant
+
+    def giulio_F(self, z):
+        r = distance(z, self._self._center)
         return r * math.erf(r/self._alpha) + (self._alpha/math.sqrt(pi)) * math.pow(e, -(r/self._alpha) ** 2)
 
-    def gaussian_F(z, center, alpha, constant):
-        r = distance(z, center)
-        return -math.pow(e, -alpha**2 * r**2) / (2 * alpha**2) + constant
+    def gaussian_F(self, z):
+        r = distance(z, self._center)
+        return -math.pow(e, -self._alpha**2 * r**2) / (2 * self._alpha**2) + self._constant
 
-    def multiquadric_F(z, center, alpha, constant):
-        r = distance(z, center)
-        return math.pow(alpha**2 * r**2 + 1, 3/2) / (3 * alpha**2) + constant
+    def multiquadric_F(self, z):
+        r = distance(z, self._center)
+        return math.pow(self._alpha**2 * r**2 + 1, 3/2) / (3 * self._alpha**2) + self._constant
 
-    def inverseQuadratic_F(z, center, alpha, constant):
-        r = distance(z, center)
-        return math.log(alpha**2 * r**2 + 1) / (2 * alpha**2) + constant
+    def inverseQuadratic_F(self, z):
+        r = distance(z, self._center)
+        return math.log(self._alpha**2 * r**2 + 1) / (2 * self._alpha**2) + self._constant
 
-    def inverseMultiquadric_F(z, center, alpha, constant):
-        r = distance(z, center)
-        return math.sqrt(alpha**2 * r**2 + 1) / (alpha**2) + constant
+    def inverseMultiquadric_F(self, z):
+        r = distance(z, self._center)
+        return math.sqrt(self._alpha**2 * r**2 + 1) / (self._alpha**2) + self._constant
 
-    def polyharmonicSpline_F(z, center, constant, k):
-        r = distance(z, center)
-        if k % 2 == 0:
+    def polyharmonicSpline_F(self, z):
+        r = distance(z, self._center)
+        if self._k % 2 == 0:
             # Since k and r are positive
-            return (r**(k+1) * ((k+2) * math.log(r) - 1)) / ((k+2)**2) + constant
+            return (r**(self._k+1) * ((self._k+2) * math.log(r) - 1)) / ((self._k+2)**2) + self._constant
         else:
-            return r**(k+2) / (k+2) + constant
+            return r**(self._k+2) / (self._k+2) + self._constant
 
-    def thinPlateSpline_F(z, center, constant):
-        r = distance(z, center)
-        return r**4 * (math.log(r)/4 - 1/16) + constant
+    def thinPlateSpline_F(self, z):
+        r = distance(z, self._center)
+        return r**4 * (math.log(r)/4 - 1/16) + self._constant
