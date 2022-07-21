9629e5204924e7692808e576b92f033926411b2b
AlfinSib <s2150442@ed.ac.uk>
2022-07-21 23:59:56 +0100

    Simplify the code using the method shown in 2d

--- nD/nD Implementation (Newton's Method).py
+++ nD/nD Implementation (Newton's Method).py
@@ -22,115 +22,36 @@ def distance(z1, z2):
 
 
 def BetaNewton(): # Newton's method (Experimental)
-    xSummationGradient_1 = 0
-    ySummationGradient_1 = 0
-    xSummationGradient_2 = 0
-    ySummationGradient_2 = 0
-    xSummationGradient_3 = 0
-    xSummationGradient_4 = 0
-    xSummationGradient_5 = 0
-    ySummationGradient_3 = 0
-    ySummationGradient_4 = 0
-    ySummationGradient_5 = 0
+    xSummationGradient = [0, 0, 0, 0, 0]
+    ySummationGradient = [0, 0, 0, 0, 0]
+    G = [0, 0, 0, 0, 0]
     for i in range(0, len(MixtureSample)):
-        xSummationGradient_1 += PotentialF_1.giulio_F(MixtureSample[i])
-        xSummationGradient_2 += PotentialF_2.gaussian_F(MixtureSample[i])
-        xSummationGradient_3 += PotentialF_3.multiquadric_F(MixtureSample[i])
-        xSummationGradient_4 += PotentialF_4.inverseQuadratic_F(MixtureSample[i])
-        xSummationGradient_5 += PotentialF_5.inverseMultiquadric_F(MixtureSample[i])
+        xSummationGradient[0] += PotentialF_1.giulio_F(MixtureSample[i])
+        xSummationGradient[1] += PotentialF_2.gaussian_F(MixtureSample[i])
+        xSummationGradient[2] += PotentialF_3.multiquadric_F(MixtureSample[i])
+        xSummationGradient[3] += PotentialF_4.inverseQuadratic_F(MixtureSample[i])
+        xSummationGradient[4] += PotentialF_5.inverseMultiquadric_F(MixtureSample[i])
     for j in range(0, len(StandardNormal)):
-        ySummationGradient_1 += PotentialF_1.giulio_F(StandardNormal[j])
-        ySummationGradient_2 += PotentialF_2.gaussian_F(StandardNormal[j])
-        ySummationGradient_3 += PotentialF_3.multiquadric_F(StandardNormal[j])
-        ySummationGradient_4 += PotentialF_4.inverseQuadratic_F(StandardNormal[j])
-        ySummationGradient_5 += PotentialF_5.inverseMultiquadric_F(StandardNormal[j])
-    G_1 = (1/len(MixtureSample)) * xSummationGradient_1 - (1/len(StandardNormal)) * ySummationGradient_1
-    G_2 = (1/len(MixtureSample)) * xSummationGradient_2 - (1/len(StandardNormal)) * ySummationGradient_2
-    G_3 = (1/len(MixtureSample)) * xSummationGradient_3 - (1/len(StandardNormal)) * ySummationGradient_3
-    G_4 = (1/len(MixtureSample)) * xSummationGradient_4 - (1/len(StandardNormal)) * ySummationGradient_4
-    G_5 = (1/len(MixtureSample)) * xSummationGradient_5 - (1/len(StandardNormal)) * ySummationGradient_5
-    G = np.array([G_1, G_2, G_3, G_4, G_5])
-    yHessian_11 = 0
-    yHessian_12 = 0
-    yHessian_13 = 0
-    yHessian_14 = 0
-    yHessian_15 = 0
-    yHessian_21 = 0
-    yHessian_22 = 0
-    yHessian_23 = 0
-    yHessian_24 = 0
-    yHessian_25 = 0
-    yHessian_31 = 0
-    yHessian_32 = 0
-    yHessian_33 = 0
-    yHessian_34 = 0
-    yHessian_35 = 0
-    yHessian_41 = 0
-    yHessian_42 = 0
-    yHessian_43 = 0
-    yHessian_44 = 0
-    yHessian_45 = 0
-    yHessian_51 = 0
-    yHessian_52 = 0
-    yHessian_53 = 0
-    yHessian_54 = 0
-    yHessian_55 = 0
+        ySummationGradient[0] += PotentialF_1.giulio_F(StandardNormal[j])
+        ySummationGradient[1] += PotentialF_2.gaussian_F(StandardNormal[j])
+        ySummationGradient[2] += PotentialF_3.multiquadric_F(StandardNormal[j])
+        ySummationGradient[3] += PotentialF_4.inverseQuadratic_F(StandardNormal[j])
+        ySummationGradient[4] += PotentialF_5.inverseMultiquadric_F(StandardNormal[j])
+    for k in range(0, 5):
+        G[k] = (1/len(MixtureSample)) * xSummationGradient[k] - (1/len(StandardNormal)) * ySummationGradient[k]
+    G = np.array(G)
+    yHessian = np.zeros([5,5])
     for l in range(0, len(StandardNormal)):
-        yHessian_11 += np.dot(nd.Gradient(PotentialF_1.giulio_F)([StandardNormal[l]]), nd.Gradient(PotentialF_1.giulio_F)(StandardNormal[l]))
-        yHessian_12 += np.dot(nd.Gradient(PotentialF_1.giulio_F)([StandardNormal[l]]), nd.Gradient(PotentialF_2.gaussian_F)(StandardNormal[l]))
-        yHessian_13 += np.dot(nd.Gradient(PotentialF_1.giulio_F)([StandardNormal[l]]), nd.Gradient(PotentialF_3.multiquadric_F)(StandardNormal[l]))
-        yHessian_14 += np.dot(nd.Gradient(PotentialF_1.giulio_F)([StandardNormal[l]]), nd.Gradient(PotentialF_4.inverseQuadratic_F)(StandardNormal[l]))
-        yHessian_15 += np.dot(nd.Gradient(PotentialF_1.giulio_F)([StandardNormal[l]]), nd.Gradient(PotentialF_5.inverseMultiquadric_F)(StandardNormal[l]))
-        yHessian_21 += np.dot(nd.Gradient(PotentialF_2.gaussian_F)([StandardNormal[l]]), nd.Gradient(PotentialF_1.giulio_F)(StandardNormal[l]))
-        yHessian_22 += np.dot(nd.Gradient(PotentialF_2.gaussian_F)([StandardNormal[l]]), nd.Gradient(PotentialF_2.gaussian_F)(StandardNormal[l]))
-        yHessian_23 += np.dot(nd.Gradient(PotentialF_2.gaussian_F)([StandardNormal[l]]), nd.Gradient(PotentialF_3.multiquadric_F)(StandardNormal[l]))
-        yHessian_24 += np.dot(nd.Gradient(PotentialF_2.gaussian_F)([StandardNormal[l]]), nd.Gradient(PotentialF_4.inverseQuadratic_F)(StandardNormal[l]))
-        yHessian_25 += np.dot(nd.Gradient(PotentialF_2.gaussian_F)([StandardNormal[l]]), nd.Gradient(PotentialF_5.inverseMultiquadric_F)(StandardNormal[l]))
-        yHessian_31 += np.dot(nd.Gradient(PotentialF_3.multiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_1.giulio_F)(StandardNormal[l]))
-        yHessian_32 += np.dot(nd.Gradient(PotentialF_3.multiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_2.gaussian_F)(StandardNormal[l]))
-        yHessian_33 += np.dot(nd.Gradient(PotentialF_3.multiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_3.multiquadric_F)(StandardNormal[l]))
-        yHessian_34 += np.dot(nd.Gradient(PotentialF_3.multiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_4.inverseQuadratic_F)(StandardNormal[l]))
-        yHessian_35 += np.dot(nd.Gradient(PotentialF_3.multiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_5.inverseMultiquadric_F)(StandardNormal[l]))
-        yHessian_41 += np.dot(nd.Gradient(PotentialF_4.inverseQuadratic_F)([StandardNormal[l]]), nd.Gradient(PotentialF_1.giulio_F)(StandardNormal[l]))
-        yHessian_42 += np.dot(nd.Gradient(PotentialF_4.inverseQuadratic_F)([StandardNormal[l]]), nd.Gradient(PotentialF_2.gaussian_F)(StandardNormal[l]))
-        yHessian_43 += np.dot(nd.Gradient(PotentialF_4.inverseQuadratic_F)([StandardNormal[l]]), nd.Gradient(PotentialF_3.multiquadric_F)(StandardNormal[l]))
-        yHessian_44 += np.dot(nd.Gradient(PotentialF_4.inverseQuadratic_F)([StandardNormal[l]]), nd.Gradient(PotentialF_4.inverseQuadratic_F)(StandardNormal[l]))
-        yHessian_45 += np.dot(nd.Gradient(PotentialF_4.inverseQuadratic_F)([StandardNormal[l]]), nd.Gradient(PotentialF_5.inverseMultiquadric_F)(StandardNormal[l]))
-        yHessian_51 += np.dot(nd.Gradient(PotentialF_5.inverseMultiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_1.giulio_F)(StandardNormal[l]))
-        yHessian_52 += np.dot(nd.Gradient(PotentialF_5.inverseMultiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_2.gaussian_F)(StandardNormal[l]))
-        yHessian_53 += np.dot(nd.Gradient(PotentialF_5.inverseMultiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_3.multiquadric_F)(StandardNormal[l]))
-        yHessian_54 += np.dot(nd.Gradient(PotentialF_5.inverseMultiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_4.inverseQuadratic_F)(StandardNormal[l]))
-        yHessian_55 += np.dot(nd.Gradient(PotentialF_5.inverseMultiquadric_F)([StandardNormal[l]]), nd.Gradient(PotentialF_5.inverseMultiquadric_F)(StandardNormal[l]))
-    H_11 = (1/len(StandardNormal)) * yHessian_11
-    H_12 = (1/len(StandardNormal)) * yHessian_12
-    H_13 = (1/len(StandardNormal)) * yHessian_13
-    H_14 = (1/len(StandardNormal)) * yHessian_14
-    H_15 = (1/len(StandardNormal)) * yHessian_15
-    H_21 = (1/len(StandardNormal)) * yHessian_21
-    H_22 = (1/len(StandardNormal)) * yHessian_22
-    H_23 = (1/len(StandardNormal)) * yHessian_23
-    H_24 = (1/len(StandardNormal)) * yHessian_24
-    H_25 = (1/len(StandardNormal)) * yHessian_25
-    H_31 = (1/len(StandardNormal)) * yHessian_31
-    H_32 = (1/len(StandardNormal)) * yHessian_32
-    H_33 = (1/len(StandardNormal)) * yHessian_33
-    H_34 = (1/len(StandardNormal)) * yHessian_34
-    H_35 = (1/len(StandardNormal)) * yHessian_35
-    H_41 = (1/len(StandardNormal)) * yHessian_41
-    H_42 = (1/len(StandardNormal)) * yHessian_42
-    H_43 = (1/len(StandardNormal)) * yHessian_43
-    H_44 = (1/len(StandardNormal)) * yHessian_44
-    H_45 = (1/len(StandardNormal)) * yHessian_45
-    H_51 = (1/len(StandardNormal)) * yHessian_51
-    H_52 = (1/len(StandardNormal)) * yHessian_52
-    H_53 = (1/len(StandardNormal)) * yHessian_53
-    H_54 = (1/len(StandardNormal)) * yHessian_54
-    H_55 = (1/len(StandardNormal)) * yHessian_55
-    H = np.array([[H_11,H_12,H_13,H_14,H_15],
-                 [H_21,H_22,H_23,H_24,H_25],
-                 [H_31,H_32,H_33,H_34,H_35],
-                 [H_41,H_42,H_43,H_44,H_45],
-                 [H_51,H_52,H_53,H_54,H_55]])
+        F_gradient = [nd.Gradient(PotentialF_1.giulio_F)([StandardNormal[l]]),
+        nd.Gradient(PotentialF_2.gaussian_F)(StandardNormal[l]),
+        nd.Gradient(PotentialF_3.multiquadric_F)(StandardNormal[l]),
+        nd.Gradient(PotentialF_4.inverseQuadratic_F)(StandardNormal[l]),
+        nd.Gradient(PotentialF_5.inverseMultiquadric_F)(StandardNormal[l])]
+        for m in range(0, 5):
+            for n in range(0, 5):
+                yHessian[m][n] += np.dot(F_gradient[m], F_gradient[n])
+    
+    H = np.multiply(yHessian, 1/len(StandardNormal))
     HInverseNeg = (-1) * np.linalg.inv(H)
     Beta = np.matmul(HInverseNeg, G)
     LearningRate = 0.01 # Not sure how to choose this value
@@ -239,7 +160,7 @@ Iteration = 0
 for i in range(0, 10): # Maybe there is a problem of overfitting
     Iteration += 1
     CenterList = []
-    for i in range(0,5):
+    for j in range(0,5):
         CenterList.append(CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)])
     PotentialF_1.set_center(CenterList[0])
     PotentialF_2.set_center(CenterList[1])
