e4b4cabf620242f277aa5faa2f68774580229ddd
AlfinSib <s2150442@ed.ac.uk>
2022-07-21 01:09:54 +0100

    Multiple Changes (See Description) 1. Choose different centers for different Fs 2. Improve the program spped by simplifying calculation 3. Choose a LearningRate that ensures stable decrease of LL/D

--- 2D Implementation (Newton Variation).py
+++ 2D Implementation (Newton Variation).py
@@ -5,6 +5,8 @@ import numdifftools as nd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+random.seed(0)
+np.random.seed(0)
 
 e = math.e
 pi = math.pi
@@ -16,21 +18,47 @@ def distance(z1, z2):
     return math.sqrt(sum)
 
 def F_1(z):
-    r = distance(z, center)
-    alpha = 0.5 # Consistent with the density (As this gets larger, less samples are moved close to 0)
+    r = distance(z, center_1)
+    alpha = 0.75 # Consistent with the density (As this gets larger, less samples are moved close to 0)
     return r * math.erf(r/alpha) + (alpha/math.sqrt(pi)) * math.pow(e, -(r/alpha) ** 2)
 
 def F_2(z):
-    r = distance(z, center)
-    alpha = 0.5
+    r = distance(z, center_2)
+    alpha = 0.75
     return alpha + r - alpha * math.log(abs(alpha + r))
 
 def BetaNewton(): # Newton's method (Experimental)
-    H = nd.Hessian(D)([0, 0])
-    G = nd.Gradient(D)([0, 0])
+    xSummationGradient_1 = 0
+    ySummationGradient_1 = 0
+    xSummationGradient_2 = 0
+    ySummationGradient_2 = 0
+    for i in range(0, len(MixtureSample)):
+        xSummationGradient_1 += F_1(MixtureSample[i])
+        xSummationGradient_2 += F_2(MixtureSample[i])
+    for j in range(0, len(StandardNormal)):
+        ySummationGradient_1 += F_1(StandardNormal[j])
+        ySummationGradient_2 += F_2(StandardNormal[j])
+    G_1 = (1/len(MixtureSample)) * xSummationGradient_1 - (1/len(StandardNormal)) * ySummationGradient_1
+    G_2 = (1/len(MixtureSample)) * xSummationGradient_2 - (1/len(StandardNormal)) * ySummationGradient_2
+    G = np.array([G_1, G_2])
+    yHessian_11 = 0
+    yHessian_12 = 0
+    yHessian_21 = 0
+    yHessian_22 = 0
+    for l in range(0, len(StandardNormal)):
+        yHessian_11 += np.dot(nd.Gradient(F_1)([StandardNormal[l]]), nd.Gradient(F_1)(StandardNormal[l]))
+        yHessian_12 += np.dot(nd.Gradient(F_1)([StandardNormal[l]]), nd.Gradient(F_2)(StandardNormal[l]))
+        yHessian_21 += np.dot(nd.Gradient(F_2)([StandardNormal[l]]), nd.Gradient(F_1)(StandardNormal[l]))
+        yHessian_22 += np.dot(nd.Gradient(F_2)([StandardNormal[l]]), nd.Gradient(F_2)(StandardNormal[l]))
+    H_11 = (1/len(StandardNormal)) * yHessian_11
+    H_12 = (1/len(StandardNormal)) * yHessian_12
+    H_21 = (1/len(StandardNormal)) * yHessian_21
+    H_22 = (1/len(StandardNormal)) * yHessian_22
+    H = np.array([[H_11,H_12],
+                 [H_21,H_22]])
     HInverseNeg = (-1) * np.linalg.inv(H)
     Beta = np.matmul(HInverseNeg, G)
-    LearningRate = 1 # Not sure how to choose this value
+    LearningRate = 0.01 # Not sure how to choose this value
     ParameterList = [1, LearningRate/np.linalg.norm(Beta)]
     return Beta * min(ParameterList) # min(ParameterList) can be understood as similar to the "Proportion" in gradient descent
 
@@ -73,10 +101,10 @@ def MixtureSampleGenerator():
     cov1 = [[0.5, 0], [0, 0.5]]
     mean2 = [-1, 1]
     cov2 = [[0.5, 0], [0, 0.5]]
-    x = np.random.multivariate_normal(mean1, cov1, 50)
-    y = np.random.multivariate_normal(mean2, cov2, 50)
+    x = np.random.multivariate_normal(mean1, cov1, 100)
+    y = np.random.multivariate_normal(mean2, cov2, 100)
     MixtureSample = []
-    for i in range(50):
+    for i in range(100):
         RandomSelector = random.random()
         if RandomSelector > 0.7:
             MixtureSample.append(x[i])
@@ -87,9 +115,9 @@ def MixtureSampleGenerator():
 
 def StandardNormalGenerator():
     Sample = []
-    x = np.random.standard_normal(50)
-    y = np.random.standard_normal(50)
-    for i in range(50):
+    x = np.random.standard_normal(100)
+    y = np.random.standard_normal(100)
+    for i in range(100):
         Sample.append([x[i], y[i]])
     return Sample
 
@@ -116,8 +144,13 @@ Iteration = 0
 while True: # Maybe there is a problem of overfitting
     #print("Iteration " + str(i))
     Iteration += 1
-    CenterSelector = 0
-    center = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
+    center_1 = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
+    center_2 = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
+    while Iteration <= 5 and distance(center_1, center_2) <= 0.75:
+        center_1 = MixtureSample[random.randint(0, len(MixtureSample) - 1)]
+        center_2 = MixtureSample[random.randint(0, len(MixtureSample) - 1)]
+    while distance(center_1, center_2) <= 0.75:
+        center_1 = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
     Beta = BetaNewton()
     print(Beta)
     Beta_1 = Beta[0]
