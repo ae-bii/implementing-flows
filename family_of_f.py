a37fa9a8c26f2860ab424b99316302d7ffd05c7b
Anupam Bhakta <anupambhakta1@gmail.com>
2022-07-21 22:50:17 +0530

    implemented setters for all in Potentialf

--- family_of_f.py
+++ family_of_f.py
@@ -14,42 +14,56 @@ def distance(z1, z2):
 
 
 class Potentialf:
-    def bump_f(z, center, alpha):
+    def __init__(self, center=[], alpha=1, k=0):
+        self._center = center
+        self._alpha = alpha
+        self._k = k
+
+    def set_center(self, center):
+        self._center = center
+
+    def set_alpha(self, alpha):
+        self._alpha = alpha
+
+    def set_k(self, k):
+        self._k = k
+
+    def bump_f(self, z):
         value = 0
-        r = distance(z, center)
-        if r < 1/alpha:
-            value = math.pow(e, -(1/(1-(alpha*r)**2)))
+        r = distance(z, self._center)
+        if r < 1/self._alpha:
+            value = math.pow(e, -(1/(1-(self._alpha*r)**2)))
         return value
 
-    def giulio_f(z, center, alpha):
-        r = distance(z, center)
-        return math.erf(r/alpha) / r
+    def giulio_f(self, z):
+        r = distance(z, self._center)
+        return math.erf(r/self._alpha) / r
 
-    def gaussian_f(z, center, alpha):
-        r = distance(z, center)
-        return math.pow(e, -((alpha*r)**2))
+    def gaussian_f(self, z):
+        r = distance(z, self._center)
+        return math.pow(e, -((self._alpha*r)**2))
 
-    def multiquadric_f(z, center, alpha):
-        r = distance(z, center)
-        return math.sqrt(1+(alpha*r)**2)
+    def multiquadric_f(self, z):
+        r = distance(z, self._center)
+        return math.sqrt(1+(self._alpha*r)**2)
 
-    def inverseQuadratic_f(z, center, alpha):
-        r = distance(z, center)
-        return 1/(1+(alpha*r)**2)
+    def inverseQuadratic_f(self, z):
+        r = distance(z, self._center)
+        return 1/(1+(self._alpha*r)**2)
 
-    def inverseMultiquadric_f(z, center, alpha):
-        r = distance(z, center)
-        return 1/math.sqrt(1+(alpha*r)**2)
+    def inverseMultiquadric_f(self, z):
+        r = distance(z, self._center)
+        return 1/math.sqrt(1+(self._alpha*r)**2)
 
-    def polyharmonicSpline_f(z, center, k):
-        r = distance(z, center)
-        if k % 2 == 0:
-            return r**(k-1)*np.log(r**r)
+    def polyharmonicSpline_f(self, z):
+        r = distance(z, self._center)
+        if self._k % 2 == 0:
+            return r**(self._k-1)*np.log(r**r)
         else:
-            return r**k
+            return r**self._k
 
-    def thinPlateSpline_f(z, center):
-        r = distance(z, center)
+    def thinPlateSpline_f(self, z):
+        r = distance(z, self._center)
         return r**2*np.log(r)
 
 
