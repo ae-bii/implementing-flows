5cd588f132cb54b2f7e6330f5b9ee72bb9f9a941
AlfinSib <s2150442@ed.ac.uk>
2022-07-30 22:48:24 +0100

    Vectorizing in progress Use list comprehension to replace for loop

--- 2D/2D Newton's Method wth Crescent samples.py
+++ 2D/2D Newton's Method wth Crescent samples.py
@@ -30,16 +30,12 @@ def BetaNewton(): # Newton's method (Experimental)
     xSummationGradient = np.zeros(NumFs)
     ySummationGradient = np.zeros(NumFs)
     G = np.zeros(NumFs)
-    for f in range(0,NumFs):
-        xSummationGradient[f] = sum(np.apply_along_axis(PotentialFs[f],1,MixtureSample))
-        ySummationGradient[f] = sum(np.apply_along_axis(PotentialFs[f],1,CrescentSample))
-    for k in range(0, NumFs):
-        G[k] = (1/len(MixtureSample)) * xSummationGradient[k] - (1/len(CrescentSample)) * ySummationGradient[k]
+    xSummationGradient = [sum(PotentialFsVectorized[f](MixtureSample)) for f in range(NumFs)]
+    ySummationGradient = [sum(PotentialFsVectorized[f](CrescentSample)) for f in range(NumFs)]
+    G = [(1/len(MixtureSample)) * xSummationGradient[k] - (1/len(CrescentSample)) * ySummationGradient[k] for k in range(NumFs)]
     G = np.array(G)
     yHessian = np.zeros([NumFs,NumFs])
-    F_gradient = [0,0,0,0,0]
-    for f in range(0, NumFs):
-        F_gradient[f] = np.apply_along_axis(nd.Gradient(PotentialFs[f]),1,CrescentSample)
+    F_gradient = [np.apply_along_axis(nd.Gradient(PotentialFs[f]),1,CrescentSample) for f in range(NumFs)]
     for m in range(0, NumFs):
         for n in range(0, NumFs):
             yHessian[m][n] = sum(np.apply_along_axis(sum,1,F_gradient[m]*F_gradient[n]))
@@ -52,21 +48,20 @@ def BetaNewton(): # Newton's method (Experimental)
     return Beta * min(ParameterList) # min(ParameterList) can be understood as similar to the "Proportion" in gradient descent
 
 def u(x):
-    F_eval = []
-    for f in range(0,NumFs):
-        F_eval.append(PotentialFs[f](x))
+    F_eval = [(PotentialFs[f](x)) for f in range(NumFs)]
     return (((x[0] ** 2) + (x[1] ** 2)) / 2) + np.dot(Beta, F_eval)
 
+def uVectorized(x):
+    F_eval = [PotentialFsVectorized[f](x) for f in range(NumFs)]
+    return [((x[i][0] ** 2) + (x[i][1] ** 2))/2 for i in range(len(x))] + np.apply_along_axis(sum,1,np.transpose(F_eval) * Beta)
+
 def uConjugate(y):
-    ConvexCandidate = []
-    for i in range(0, len(MixtureSample)):
-        ConjugateValue = np.dot(MixtureSample[i], y) - u(MixtureSample[i])
-        ConvexCandidate.append(ConjugateValue)
+    ConvexCandidate = [(np.dot(MixtureSample[i], y) - u(MixtureSample[i])) for i in range(len(MixtureSample))]
     return max(ConvexCandidate)
 
 def D():
 
-    xSummation = sum(np.apply_along_axis(u, 1, MixtureSample))
+    xSummation = sum(uVectorized(MixtureSample))
     ySummation = sum(np.apply_along_axis(uConjugate, 1, CrescentSample))
 
     LL = 1/len(MixtureSample) * xSummation + 1 / \
@@ -84,10 +79,7 @@ def SamplesUpdate(OldMixtureSample):
         F_eval_y[f] = (gradient[:,1])
     F_eval_x = np.array(F_eval_x)
     F_eval_y= np.array(F_eval_y)
-    for i in range(0, len(OldMixtureSample)):
-        xval = OldMixtureSample[i][0] + np.dot(Beta, F_eval_x[:,i])
-        yval = OldMixtureSample[i][1] + np.dot(Beta, F_eval_y[:,i])
-        NewMixtureSample.append([xval,yval])
+    NewMixtureSample = [[OldMixtureSample[i][0] + np.dot(Beta, F_eval_x[:,i]), OldMixtureSample[i][1] + np.dot(Beta, F_eval_y[:,i])] for i in range(0, len(OldMixtureSample)) ]
     NewMixtureSample = np.array(NewMixtureSample)
     return NewMixtureSample
 
@@ -129,6 +121,11 @@ PotentialFs = [functions.Giulio_F(alpha=1),
                 functions.InverseQuadratic_F(alpha=1, constant=1),
                 functions.InverseMultiquadric_F(alpha=1, constant=1)]
 NumFs = len(PotentialFs)
+PotentialFsVectorized = [functions.Giulio_F_Vectorized(alpha = 1),
+                        functions.Gaussian_F_Vectorized(alpha=1, constant=1),
+                        functions.Multiquadric_F_Vectorized(alpha=1, constant=1),
+                        functions.InverseQuadratic_F_Vectorized(alpha=1, constant=1),
+                        functions.InverseMultiquadric_F_Vectorized(alpha=1, constant=1)]
 
 plt.subplot(1,3,3)
 plt.title("Target")
@@ -150,24 +147,26 @@ Beta = 0
 #profiler = cProfile.Profile()
 #profiler.enable()
 
-while True: # Maybe there is a problem of overfitting
+for i in range(25): # Maybe there is a problem of overfitting
     #print("Iteration " + str(i))
     Iteration += 1
     if Iteration >= 10:
         CenterGeneratorList = MixtureSample + CrescentSample
     CenterList = []
+    DistanceMixture = np.zeros([500,5])
+    DistanceTarget = np.zeros([500,5])
     for i in range(0,NumFs):
         c = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
         CenterList.append(c)
         PotentialFs[i].setCenter(c)
+        PotentialFsVectorized[i].setCenter(c)
     OldBeta = Beta
     Beta = BetaNewton()
     OldD = DValue
     DValue = D()
     print(DValue)
     MixtureSample = SamplesUpdate(MixtureSample)
-    if norm(OldBeta - Beta) < 0.0001 or Iteration > 25:
-        break
+
     
 #profiler.disable()
 #stats = pstats.Stats(profiler).sort_stats('tottime')
