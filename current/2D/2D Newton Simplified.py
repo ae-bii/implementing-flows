fefa56c9089226868fa13a7fd00a1cba67a4797f
AlfinSib <s2150442@ed.ac.uk>
2022-11-25 21:59:44 +0000

    Latest Simplified version

--- current/2D/2D Newton Simplified.py
+++ current/2D/2D Newton Simplified.py
@@ -1,44 +1,56 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-import math
 from scipy.special import erf
 import SampleGenerator as sg
+import scipy as sp
 
+random.seed(5)
 
-def radialBasisFunc(center, varList):
+# def radialBasisFunc(center, varList, bw):
+#    rList = np.linalg.norm(np.subtract(varList,center), axis=1)
+#    return np.multiply(rList,erf(np.divide(rList, bw))) + np.multiply((bw/np.sqrt(np.pi)), np.exp(-np.square(np.divide(rList, bw))))
+
+def radialBasisFunc(center, varList, bw):
     rList = np.linalg.norm(np.subtract(varList,center), axis=1)
-    return np.multiply(rList,erf(np.divide(rList, alpha))) + np.multiply((alpha/np.sqrt(np.pi)), np.exp(-np.square(np.divide(rList, alpha))))
+    return 1/(bw * np.sqrt(2*np.pi)) * np.exp((-1/2) * np.square(np.divide((rList), bw)))
 
-def radialBasisGrad(center, varList):
-    Gradient = []
-    delta = 1e-8
-    GradientX = ((radialBasisFunc(center, np.add(varList,[delta/2, 0])) - radialBasisFunc(center, np.subtract(varList,[delta/2, 0])))/delta)
-    GradientY = ((radialBasisFunc(center, np.add(varList,[0, delta/2])) - radialBasisFunc(center, np.subtract(varList,[0, delta/2])))/delta)
-    Gradient = np.c_[GradientX,GradientY]
-    return Gradient
+def radialBasisGrad(center, varList, bw):
+    diffVarCenter = np.subtract(varList,center)
+    rList = np.linalg.norm(diffVarCenter, axis=1)   
+    mainBody = 1/(bw * np.sqrt(2*np.pi)) * np.exp((-1/2) * np.square(np.divide((rList), bw))) * np.divide(-rList, (bw ** 2))
+    return  np.c_[mainBody * diffVarCenter[:,0], mainBody * diffVarCenter[:,1]]
+
+
+# def radialBasisGrad(center, varList, bw):
+#     diffVarCenter = np.subtract(varList,center)
+#     rList = np.linalg.norm(diffVarCenter, axis=1)
+#     testVar = (np.divide(erf(np.divide(rList, bw)), rList))
+#     fstCol = diffVarCenter[:,0]
+#     sndCol = diffVarCenter[:,1]    
+#     return np.c_[testVar * fstCol, testVar * sndCol]
 
 def betaNewton(initialSample,targetSample,center):
-    grad = (1/len(initialSample)) * np.sum(radialBasisFunc(initialSample,center)) - (1/len(targetSample)) * np.sum(radialBasisFunc(targetSample,center))
-    hess = (1/len(targetSample)) * np.sum((radialBasisGrad(targetSample,center) * radialBasisGrad(targetSample,center)).sum(axis = 1))
+    grad = (1/len(initialSample)) * np.sum(radialBasisFunc(initialSample,center,bw)) - (1/len(targetSample)) * np.sum(radialBasisFunc(targetSample,center,bw))
+    hess = (1/len(targetSample)) * np.sum((radialBasisGrad(targetSample,center,bw) * radialBasisGrad(targetSample,center,bw)).sum(axis = 1)) #
     betaRaw = -(1/hess) * grad
     learningRate = [1, 0.5/abs(betaRaw)]
     betaAdjusted = betaRaw * min(learningRate)
-    print(betaAdjusted)
     return betaAdjusted
 
 def sampleUpdate(center, varList,beta):    
-    rbfGrad = radialBasisGrad(center, varList)
+    rbfGrad = radialBasisGrad(center, varList,bw)
     rbfEvalX = np.array(rbfGrad[:,0])
     rbfEvalY = np.array(rbfGrad[:,1])
-    xVal = varList[:,0] 
+    xVal = varList[:,0] + (np.multiply(rbfEvalX, beta))
     yVal = varList[:,1] + (np.multiply(rbfEvalY, beta))
     newVarList = np.c_[xVal,yVal]
     return newVarList
 
 targetSample = sg.JointSampleGenerator()
 initialSample = sg.IndependentCouplingGenerator(targetSample, len(targetSample))
-
+betaList = []
+hessList = []
 CenterGeneratorList = targetSample.tolist()
 
 plt.rc('axes', titlesize=15) 
@@ -48,7 +60,7 @@ plt.title("Initial (Independent Coupling)")
 plt.scatter(*zip(*initialSample), color = 'b', alpha = 0.2)
 plt.xlim(-4, 4)
 plt.ylim(-1, 6)
-CenterList = []
+
 
 plt.subplot(1,3,3)
 plt.title("Target (Joint Samples)")
@@ -56,23 +68,23 @@ plt.scatter(*zip(*targetSample), color = 'r', alpha = 0.2)
 plt.xlim(-4, 4)
 plt.ylim(-1, 6)
 
-for i in range(750): # Maybe there is a problem of overfitting
+for i in range(1000): # Maybe there is a problem of overfitting
     center = CenterGeneratorList[random.randint(0, len(CenterGeneratorList) - 1)]
-    CenterGeneratorList.remove(center)
-    if len(CenterGeneratorList) == 0:
-        CenterGeneratorList = targetSample.tolist()
-    if center[0]<2 and center[0]>-2 and center[1]<5 and center[1]>4:
-        maybeDensity = 0.8
-    else:
-        maybeDensity = 0.2
-    alpha = ((15/(len(initialSample) + len(targetSample))) * (1/(1/30) + 1/maybeDensity)) ** (1/2)
+    center += np.array([0.001,0.001])
+
+    bw = 1
     beta = betaNewton(initialSample, targetSample, center)
+    betaList.append(beta)
     initialSample = sampleUpdate(center, initialSample, beta)
 
 plt.subplot(1,3,2)
-plt.title("Flow Transport (1000 iterations)")
+plt.title("Flow Transport"+str(len(betaList))+"iterations")
 plt.scatter(*zip(*initialSample), color = 'g', alpha = 0.2)
 plt.xlim(-4, 4)
 plt.ylim(-1, 6)
+
+plt.show()
+
+plt.plot(betaList,'.')
 
 plt.show()
